1c4e60ba3b308f3f16f1ba5281dc7d0fd3cc4363
Noctis Lucis <noctislucispro@gmail.com>
2016-10-24 11:52:25 +0700

    Add giao dien

---
+++ client/MyScript/angular.js
@@ -0,0 +1,105 @@
+
+angular.module('myApp',[]).controller('myCtrl', function($scope){
+	$scope.number_value1 = ["1", "2", "3", "4", "5", "6"];
+	$scope.number_value2 = ["0", "1", "2"];
+	$scope.number_value3 = ["0", "1"];
+	$scope.danh_xung = ["Ông", "Bà"];
+	
+	$scope.chieu1 = function() 
+    {
+		 $scope.style1 = {backgroundColor: '#337ab7'};
+		 $scope.style2 = null;
+		 $scope.show = false;
+	}
+	
+	$scope.chieu2 = function() 
+    {
+		 $scope.style2 = {backgroundColor: '#337ab7'};
+		 $scope.style1 = null;
+		 $scope.show = true;
+	}
+	
+	$scope.checkNumber1 = function(selectedNumber1)
+	{
+		$scope.selectedNumber2 = $scope.number_value2[0];
+		$scope.selectedNumber3 = $scope.number_value3[0];	
+		if(selectedNumber1 == "1")
+		{	
+			$scope.number_value2 = ["0", "1", "2"];
+			$scope.number_value3 = ["0", "1"];
+		}
+		if(selectedNumber1 == "2")
+		{
+			$scope.number_value2 = ["0", "1", "2", "3", "4"];
+			$scope.number_value3 = ["0", "1", "2"];
+		}
+		if(selectedNumber1 == "3")
+		{
+			$scope.number_value2 = ["0", "1", "2", "3"];
+			$scope.number_value3 = ["0", "1", "2", "3"];
+		}
+
+		if(selectedNumber1 == "4")
+		{
+			$scope.number_value2 = ["0", "1", "2"];
+			$scope.number_value3 = ["0", "1", "2", "3", "4"];
+		}
+		
+		if(selectedNumber1 == "5")
+		{
+			$scope.number_value2 = ["0", "1"];
+			$scope.number_value3 = ["0", "1", "2", "3", "4", "5"];
+		}
+		
+		if(selectedNumber1 == "6")
+		{
+			$scope.number_value2 = ["0"];
+			$scope.number_value3 = ["0", "1", "2", "3", "4", "5", "6"];
+		}
+	}
+	
+	$scope.checkNumber2 = function(selectedNumber2)
+	{
+		$scope.selectedNumber3 = $scope.number_value3[0];	
+		if($scope.selectedNumber1 == "1")
+		{	
+			$scope.number_value3 = ["0", "1"];
+			if(selectedNumber2 == "2")
+				$scope.number_value3 = ["0"];
+		}
+		
+		if($scope.selectedNumber1 == "2")
+		{		
+			$scope.number_value3 = ["0", "1", "2"];
+			if(selectedNumber2 == "3")
+				$scope.number_value3 = ["0", "1"];
+			if(selectedNumber2 == "4")
+				$scope.number_value3 = ["0"];		
+		}
+		
+		if($scope.selectedNumber1 == "3")
+		{		
+			$scope.number_value3 = ["0", "1", "2", "3"];
+		}
+		
+		if($scope.selectedNumber1 == "4")
+		{		
+			$scope.number_value3 = ["0", "1", "2", "3", "4"];
+		}
+		
+		if($scope.selectedNumber1 == "5")
+		{		
+			$scope.number_value3 = ["0", "1", "2", "3", "4", "5"];
+		}
+		
+		if($scope.selectedNumber1 == "6")
+		{		
+			$scope.number_value3 = ["0", "1", "2", "3", "4", "5", "6"];
+		}
+	}
+	
+});
+
+
+
+	
