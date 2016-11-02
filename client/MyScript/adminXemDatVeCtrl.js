b4a4795fc77df9132e8ea54f62e15951800a819a
Danh Dang <dtdanh03@gmail.com>
2016-11-02 21:59:29 +0700

    z

--- client/MyScript/adminXemDatVeCtrl.js
+++ client/MyScript/adminXemDatVeCtrl.js
@@ -6,4 +6,17 @@ myapp.controller('adminXemDatVeCtrl',  ['$scope', '$http', 'Data', '$location', 
     $scope.themChuyenBay= function(){
     	$location.path('/admin-them-chuyen-bay');
     }
+
+    $http({
+        method: 'GET',
+        url: '/api/bookings',
+        params : {
+        	'token': Data.token
+        }
+    }).then(function successCallback(response) {
+        $scope.flights = response.data;
+        
+    }, function errorCallback(response) {
+        console.log('getting failed');
+    });
 }]);
